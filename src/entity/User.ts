a1ec14cea5b02f052404f3d976bb004a35c00a71
yagnesh16697 <40670389+yagnesh16697@users.noreply.github.com>
2021-05-13 10:21:17 +0530

    Add files via upload

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,56 +1,49 @@
 import { IsEmail, Length } from 'class-validator';
 import {
-  Entity,
-  PrimaryGeneratedColumn,
+  Entity as TOEntity,
   Column,
-  BaseEntity,
   Index,
-  CreateDateColumn,
-  UpdateDateColumn,
   BeforeInsert,
+  OneToMany,
 } from 'typeorm';
 import bcrypt from 'bcrypt';
-import { classToPlain, Exclude } from 'class-transformer';
-@Entity('users')
-export default class User extends BaseEntity {
+import { Exclude } from 'class-transformer';
+
+import Entity from './Entity';
+import Post from './Post';
+import Vote from './Vote';
+
+@TOEntity('users')
+export default class User extends Entity {
   constructor(user: Partial<User>) {
     super();
     Object.assign(this, user);
   }
-  @Exclude()
-  @PrimaryGeneratedColumn()
-  id: number;
 
   @Index()
-  @IsEmail()
+  @IsEmail(undefined, { message: 'Must be a valid email address' })
+  @Length(1, 255, { message: 'Email is empty' })
   @Column({ unique: true })
   email: string;
 
   @Index()
+  @Length(3, 255, { message: 'Must be at least 3 characters long' })
   @Column({ unique: true })
-  @Length(3, 255, {
-    message: 'Username must be at least 3 characters long',
-  })
   username: string;
 
-  @Index()
-  @Column()
-  @Length(6, 255)
   @Exclude()
+  @Column()
+  @Length(6, 255, { message: 'Must be at least 6 characters long' })
   password: string;
 
-  @CreateDateColumn()
-  createdAt: Date;
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
 
-  @UpdateDateColumn()
-  updatedAt: Date;
+  @OneToMany(() => Vote, (vote) => vote.user)
+  votes: Vote[];
 
   @BeforeInsert()
   async hashPassword() {
     this.password = await bcrypt.hash(this.password, 6);
-  }
-
-  toJSON() {
-    return classToPlain(this);
   }
 }
